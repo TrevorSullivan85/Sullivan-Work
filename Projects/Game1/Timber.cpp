33e6476e6100c882b2fd04c5328f200abfbfa916
Trevor Sullivan <trevors1775@gmail.com>
2026-09-19 16:49:31 -0400

    Display Tree, clouds, and bee on screen

--- Projects/Game1/Timber.cpp
+++ Projects/Game1/Timber.cpp
@@ -10,14 +10,43 @@ int main()
 
 	RenderWindow window(vm, "Timber!!!", Style::Fullscreen);
 
+	// Making the background
 	Texture textureBackground;
-
 	textureBackground.loadFromFile("graphics/background.png");
-
 	Sprite spriteBackground;
-
 	spriteBackground.setTexture(textureBackground);
 	spriteBackground.setPosition(0, 0);
+
+	// Making the tree
+	Texture textureTree;
+	textureTree.loadFromFile("graphics/tree.png");
+	Sprite spriteTree;
+	spriteTree.setTexture(textureTree);
+	spriteTree.setPosition(810, 0);
+
+	// Making the bee
+	Texture textureBee;
+	textureBee.loadFromFile("graphics/bee.png");
+	Sprite spriteBee;
+	spriteBee.setTexture(textureBee);
+	spriteBee.setPosition(0, 800);
+	bool beeActive = false;
+	float beeSpeed = 0.0f;
+
+	// Making 3 clouds
+	Texture textureCloud;
+	textureCloud.loadFromFile("graphics/cloud.png");
+	Sprite spriteCloud1, spriteCloud2, spriteCloud3;
+	spriteCloud1.setTexture(textureCloud);
+	spriteCloud2.setTexture(textureCloud);
+	spriteCloud3.setTexture(textureCloud);
+	// Set Positions
+	spriteCloud1.setPosition(0, 0);
+	spriteCloud2.setPosition(0, 250);
+	spriteCloud3.setPosition(0, 500);
+	bool cloud1Active = false, cloud2Active = false, cloud3Active = false;
+	float cloud1Speed = 0.0f, cloud2Speed = 0.0f, cloud3Speed = 0.0f;
+
 
 	while (window.isOpen()) {
 		if (Keyboard::isKeyPressed(Keyboard::Escape)) {
@@ -26,7 +55,19 @@ int main()
 
 		window.clear();
 
+		// Draw BG
 		window.draw(spriteBackground);
+
+		// Draw Clouds
+		window.draw(spriteCloud1);
+		window.draw(spriteCloud2);
+		window.draw(spriteCloud3);
+
+		// Draw Tree
+		window.draw(spriteTree);
+
+		// Draw Bee
+		window.draw(spriteBee);
 
 		window.display();
 
